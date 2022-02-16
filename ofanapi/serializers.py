675b7a3db42e902ed00bfc6527277eec5e5fbc84
Erick Kiminza <kiminzaeric@gmail.com>
2022-02-16 18:09:03 +0300

    add serializer for add models

--- ofanapi/serializers.py
+++ ofanapi/serializers.py
@@ -1,4 +1,4 @@
-from .models import Homes
+from .models import Homes, Groups, Join_Group
 from rest_framework import serializers
 
 
@@ -7,4 +7,18 @@ class HomesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         """ Return a list of all fields in model Homes"""
         model = Homes
+        fields = '__all__'
+
+
+class GroupsSerializer(serializers.HyperlinkedModelSerializer):
+    """ serialize Groups models"""
+    class Meta:
+        model = Groups
+        fields = '__all__'
+
+
+class Join_GroupSerializer(serializers.HyperlinkedModelSerializer):
+    """ serialize Groups models"""
+    class Meta:
+        model = Join_Group
         fields = '__all__'

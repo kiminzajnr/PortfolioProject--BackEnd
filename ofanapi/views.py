d644fce7429d91e94dd182c03dfa449821841796
Erick Kiminza <kiminzaeric@gmail.com>
2022-02-16 18:09:54 +0300

    add view for added models

--- ofanapi/views.py
+++ ofanapi/views.py
@@ -1,10 +1,26 @@
 from rest_framework import viewsets
 
-from .serializers import HomesSerializer
-from .models import Homes
+from .serializers import (
+    HomesSerializer,
+    GroupsSerializer,
+    Join_GroupSerializer
+)
+from .models import Homes, Groups, Join_Group
 
 
 class HomesViewSet(viewsets.ModelViewSet):
     """ viewset for Homes model"""
     serializer_class = HomesSerializer
     queryset = Homes.objects.all()
+
+
+class GroupsViewSet(viewsets.ModelViewSet):
+    """ viewset for Groups model"""
+    serializer_class = GroupsSerializer
+    queryset = Groups.objects.all()
+
+
+class Join_GroupViewSet(viewsets.ModelViewSet):
+    """ viewset for Join_Group model"""
+    serializer_class = Join_GroupSerializer
+    queryset = Join_Group.objects.all()

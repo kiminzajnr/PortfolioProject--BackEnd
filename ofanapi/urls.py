efbb211e7a3d279e6b4cb7d308e9eaa7a90d1c75
Erick Kiminza <kiminzaeric@gmail.com>
2022-02-16 18:09:29 +0300

    add url for added viewsets

--- ofanapi/urls.py
+++ ofanapi/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register(r'homes', views.HomesViewSet)
+router.register(r'groups', views.GroupsViewSet)
+router.register(r'Join_Group', views.Join_GroupViewSet)
 
 
 urlpatterns = router.urls
